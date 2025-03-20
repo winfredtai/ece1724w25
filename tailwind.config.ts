6d1a2b4f1e095218c63676a02bda3f74032a227d
winfredtai <17zt12@queensu.ca>
2025-03-20 18:36:37 +0800

    connect to database

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // 自定义颜色
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config; 

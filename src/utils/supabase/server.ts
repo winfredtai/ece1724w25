a871e66d45d3426aaeb5d1e10e52c3035a3d9537
winfredtai <17zt12@queensu.ca>
2025-04-03 05:19:57 +0800

    connect to new supabase

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -7,8 +7,8 @@ export async function createClient() {
   // Create a server's supabase client with newly configured cookie,
   // which could be used to maintain user's session
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.karavideo_NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.karavideo_NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
